7782bae69f7c54a5996ecdfef54cff7bf3c8e7ba
Aziret2005Ergeshov <aziretergeshov182@gmail.com>
2023-05-24 04:15:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { isDisabled } from '@testing-library/user-event/dist/utils'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+    const [title, setValue] = useState('')
+    // const inputHandler = (event) => {
+    //     setInput(event.target.value)
 
+    // }
+    const valueHandler = (e) =>{
+        let value = e.target.value
+        if(value <101){
+            setValue(value)
+        }
+    }
 
     return (
         <div className="App">
-            hello aziret
+            <input max={100} onChange={valueHandler} className='input' type='number'></input>
+            <div className='div'>
+                <div className='div2'
+                    style={{height: `${title}%`,
+                    }}></div>
+            </div>
         </div>
     )
 }
